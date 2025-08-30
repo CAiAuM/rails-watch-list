473b9f0d6dbe748bea5844ebcfb18a4cb54b13d5
Caio Figueiredo <47697760+CAiAuM@users.noreply.github.com>
2025-08-30 02:34:34 -0300

    fronted finished

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :image
 
-  validates :name, :image, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :image, presence: true
 end
